ffa0341f5230e639c2af5ac53256c68a46ad7f8e
Vlad <vlad.mark-2013@mail.ru>
2019-02-05 03:45:17 +0300

    done

---
+++ January 27/Class/RandomNumber.hpp
@@ -0,0 +1,14 @@
+//
+//  RandomNumber.hpp
+//  January 27
+//
+//  Created by vlad on 05.02.2019.
+//  Copyright © 2019 vlad. All rights reserved.
+//
+
+#ifndef RandomNumber_hpp
+#define RandomNumber_hpp
+
+#include <stdio.h>
+
+#endif /* RandomNumber_hpp */
